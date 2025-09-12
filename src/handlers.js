d1bb9663821b363561f406850498518c323952bd
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-09-13 00:59:12 +1000

    Add Japanese and english language handlers

--- src/handlers.js
+++ src/handlers.js
@@ -115,6 +115,8 @@ exports.addDefaults = /** @type Parser */ parser => {
 
     // Language
     parser.addHandler("language", /\bRUS\b/i, { type: "lowercase" });
+    parser.addHandler("language", /\bJPN\b/i, { type: "lowercase" });
+    parser.addHandler("language", /\bENG\b/i, { type: "lowercase" });
     parser.addHandler("language", /\bNL\b/, { type: "lowercase" });
     parser.addHandler("language", /\bFLEMISH\b/, { type: "lowercase" });
     parser.addHandler("language", /\bGERMAN\b/, { type: "lowercase" });
